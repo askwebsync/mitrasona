b8c412ad09b042a3ce4a575b28a4e45e07311b19
rafikarim1717 <karemrafi@gmail.com>
2023-09-13 13:55:44 +0700

    test 123

--- js/script.js
+++ js/script.js
@@ -9,7 +9,43 @@ const generalInsuranceSubmenu = document.getElementById(
 );
 const contractorSubmenu = document.getElementById("contractor-submenu");
 
+// Get the current URL
+const currentUrl = window.location.pathname;
+
+// Find all your navigation links
+const navLinks = document.querySelectorAll("nav a");
+
+// Loop through the links to find the active one based on the URL
+navLinks.forEach((link) => {
+  const linkHref = link.getAttribute("href");
+
+  // Check if the current URL contains the link's href
+  if (
+    currentUrl === linkHref ||
+    (currentUrl.startsWith(linkHref) && linkHref !== "/")
+  ) {
+    link.classList.add("active-navbar"); // Add the 'active' class to the active link
+  }
+});
+
 let isDropdownOpen = false; // Track if the dropdown menu is open
+
+const mapIframePlaceholder = document.querySelector(".map-iframe-placeholder");
+if (mapIframePlaceholder) {
+  const mapSrc = mapIframePlaceholder.getAttribute("data-src");
+  if (mapSrc) {
+    const mapIframe = document.createElement("iframe");
+    mapIframe.className = "map-iframe";
+    mapIframe.src = mapSrc;
+    mapIframe.frameborder = "0";
+    mapIframe.scrolling = "no";
+    mapIframe.allowfullscreen = true;
+    mapIframePlaceholder.parentNode.replaceChild(
+      mapIframe,
+      mapIframePlaceholder
+    );
+  }
+}
 
 mobileMenuToggle.addEventListener("click", () => {
   menu.classList.toggle("hidden");
