eaa86014cf02b1d843eea43af56dfac0b757d11b
rafikarim1717 <karemrafi@gmail.com>
2023-09-15 20:30:44 +0700

    finishing

--- js/script.js
+++ js/script.js
@@ -102,3 +102,18 @@ javascriptBarcodeReader({
   .catch((err) => {
     console.log(err);
   });
+
+function truncateText(selector, maxLength) {
+  const elements = document.querySelectorAll(selector);
+
+  elements.forEach((element) => {
+    const text = element.textContent;
+    if (text.length > maxLength) {
+      const truncatedText = text.substring(0, maxLength - 3) + "...";
+      element.textContent = truncatedText;
+    }
+  });
+}
+
+// Call the function for the specific elements you want to truncate
+truncateText(".engineering-page-card-description", 100); // Adjust 100 to your desired maximum length
